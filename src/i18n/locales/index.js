7bef58726a8f1098f26dfb72b0f0e2f1bd237a14
darde <pablodarde@gmail.com>
2018-12-13 14:20:53 -0200

    Fix unresolved paths

--- src/i18n/locales/index.js
+++ src/i18n/locales/index.js
@@ -7,30 +7,18 @@ import homeEs from '../../components/Home/locales/default.es.json';
 import augustinePt from '../../components/lighthouses/Augustine/locales/default.json';
 import augustineEn from '../../components/lighthouses/Augustine/locales/default.en.json';
 import augustineEs from '../../components/lighthouses/Augustine/locales/default.es.json';
-import herculesPt from '../../components/lighthouses/Hercules/locales/default.json';
-import herculesEn from '../../components/lighthouses/Hercules/locales/default.en.json';
-import herculesEs from '../../components/lighthouses/Hercules/locales/default.es.json';
-import lindauPt from '../../components/lighthouses/Lindau/locales/default.json';
-import lindauEn from '../../components/lighthouses/Lindau/locales/default.en.json';
-import lindauEs from '../../components/lighthouses/Lindau/locales/default.es.json';
 import peggysPt from '../../components/lighthouses/Peggys/locales/default.json';
 import peggysEn from '../../components/lighthouses/Peggys/locales/default.en.json';
 import peggysEs from '../../components/lighthouses/Peggys/locales/default.es.json';
 
 pt.home = homePt;
 pt.lighthouses.augustine = augustinePt;
-pt.lighthouses.hercules = herculesPt;
-pt.lighthouses.lindau = lindauPt;
 pt.lighthouses.peggys = peggysPt;
 en.home = homeEn;
 en.lighthouses.augustine = augustineEn;
-en.lighthouses.hercules = herculesEn;
-en.lighthouses.lindau = lindauEn;
 en.lighthouses.peggys = peggysEn;
 es.home = homeEs;
 es.lighthouses.augustine = augustineEs;
-es.lighthouses.hercules = herculesEs;
-es.lighthouses.lindau = lindauEs;
 es.lighthouses.peggys = peggysEs;
 
 export {
